8eeecb24bd65684a4c1f8e0eb06df4181117b21f
yu <yusijiax@gmail.com>
2018-07-18 10:45:15 +0800

    日志打印

--- service-1/src/main/java/com/github/silho/test/service1/controller/TestController.java
+++ service-1/src/main/java/com/github/silho/test/service1/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.github.silho.test.service1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class TestController {
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     private final String profile;
 
     @Autowired
@@ -18,6 +22,7 @@ public class TestController {
 
     @GetMapping("getProfile")
     public String getProfile() {
+        logger.debug("服务接口getProfile被调用");
         return profile;
     }
 }
