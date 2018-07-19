0bea6323c420392e626d7fa59fb6cadcbed6ed83
yu <yusijiax@gmail.com>
2018-07-19 10:37:37 +0800

    EnableDiscoveryClient配置

--- feign-1/src/main/java/com/github/silho/test/feign1/Feign1Application.java
+++ feign-1/src/main/java/com/github/silho/test/feign1/Feign1Application.java
@@ -2,11 +2,11 @@ package com.github.silho.test.feign1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Feign1Application {
 
