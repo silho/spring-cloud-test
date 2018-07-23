818020331ef6fa0363c7fd947e36bd3700c83daa
yu <yusijiax@gmail.com>
2018-07-23 19:41:50 +0800

    模拟鉴权返回失败说明

--- cloud-gateway/src/main/java/com/github/silho/cloudgateway/filters/AuthGlobalFilter.java
+++ cloud-gateway/src/main/java/com/github/silho/cloudgateway/filters/AuthGlobalFilter.java
@@ -5,12 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 
 @Component
 public class AuthGlobalFilter implements GlobalFilter, Ordered {
@@ -29,8 +34,11 @@ public class AuthGlobalFilter implements GlobalFilter, Ordered {
         // 权限验证不通过
         logger.info("权限验证不通过：{}", request.getURI().getRawPath());
         ServerHttpResponse response = exchange.getResponse();
-        response.setStatusCode(HttpStatus.UNAUTHORIZED);
-        return response.setComplete();
+        response.setStatusCode(UNAUTHORIZED);
+        response.getHeaders().setContentType(APPLICATION_JSON_UTF8);
+        byte[] bytes = "{\"code\":401,\"msg\":\"用户无权限\",\"obj\":null}".getBytes(UTF_8);
+        DataBuffer buffer = response.bufferFactory().wrap(bytes);
+        return response.writeWith(Flux.just(buffer));
     }
 
     @Override
