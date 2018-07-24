5386972eae810ae6f587a5d37dd743e16b631068
yu <yusijiax@gmail.com>
2018-07-24 19:35:50 +0800

    header example

--- feign-1/src/main/java/com/github/silho/test/feign1/remote/SpringCloudService1.java
+++ feign-1/src/main/java/com/github/silho/test/feign1/remote/SpringCloudService1.java
@@ -2,10 +2,18 @@ package com.github.silho.test.feign1.remote;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.HashMap;
 
 @FeignClient("spring-cloud-service1")
 public interface SpringCloudService1 {
 
     @GetMapping("/info/getProfile")
     String getProfileInfo();
+
+    @PostMapping("/example/header")
+    HashMap testHeader(@RequestBody HashMap<String, Object> reqMap, @RequestHeader("X-Auth-Token") String token);
 }
