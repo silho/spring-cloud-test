7fdb77652dfc1e691ecc66a96bdad214d13e853a
yu <yusijiax@gmail.com>
2018-07-24 17:50:40 +0800

    header example

---
+++ service-1/src/main/java/com/github/silho/test/service1/controller/ExampleController.java
@@ -0,0 +1,22 @@
+package com.github.silho.test.service1.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+
+@RestController("example")
+public class ExampleController {
+    private static final Logger logger = LoggerFactory.getLogger(ExampleController.class);
+
+    @PostMapping("header")
+    public HashMap header(@RequestBody HashMap<String, String> reqMap, HttpServletRequest request) {
+        logger.debug("X-Auth-Token:{}", request.getHeader("X-Auth-Token"));
+        logger.debug("请求参数:{}", reqMap);
+        return reqMap;
+    }
+}
