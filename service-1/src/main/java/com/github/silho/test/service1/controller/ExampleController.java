65b57b1261093e37bb3d948fc57a721adc3fdae9
yu <yusijiax@gmail.com>
2018-07-24 19:28:39 +0800

    修复低级错误

--- service-1/src/main/java/com/github/silho/test/service1/controller/ExampleController.java
+++ service-1/src/main/java/com/github/silho/test/service1/controller/ExampleController.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 
-@RestController("example")
+@RestController
+@RequestMapping("/example")
 public class ExampleController {
     private static final Logger logger = LoggerFactory.getLogger(ExampleController.class);
 
